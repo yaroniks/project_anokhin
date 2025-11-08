ee292eb7eef19857418ff3ee824f0d7786e77fe0
yarovich <anokhin247yarik0@gmail.com>
2025-11-08 16:56:38 +0300

    update pz_6_2, pz_6_3

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,3 +1,24 @@
 # Дан список размера N. Найти номер его первого локального минимума (локальный
 # минимум — это элемент, который меньше любого из своих соседей).
 
+try:
+    n = int(input("Введите размер списка: "))
+    nums = [int(input(f'Введите {i} элемент списка: ')) for i in range(n)]
+except ValueError:
+    print('Введите число')
+    exit()
+
+
+def find_minimum(nums: list[int]) -> int:
+    if len(nums) == 2 or len(nums) == 3:
+        if nums[0] < nums[1]:
+            return nums[0]
+        if nums[-1] < nums[-2]:
+            return nums[-1]
+
+    for i in range(1, len(nums) - 1):
+        if nums[i-1] > nums[i] < nums[i+1]:
+            return nums[i]
+
+
+print(f'Локальный минимум: {find_minimum(nums)}')
--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,3 +1,21 @@
 # Дан список размера N (N — четное число). Поменять местами его первый элемент
 # со вторым, третий — с четвертым и т. д.
 
+try:
+    n = int(input('Введите размер списка: '))
+except ValueError:
+    print('Введите число')
+    exit()
+
+if n % 2 != 0:
+    print('Число должно быть чётным')
+    exit()
+
+spisok = list(range(n))
+
+for i in range(0, len(spisok), 2):
+    now = spisok[i]
+    spisok[i] = spisok[i+1]
+    spisok[i+1] = now
+
+print(spisok)
