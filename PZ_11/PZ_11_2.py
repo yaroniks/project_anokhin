0aa44143cc4dd6f353a2495ea7eb269f96b311f3
yarovich <anokhin247yarik0@gmail.com>
2025-11-22 21:53:21 +0300

    add pz 9 10 11 12

---
+++ PZ_11/PZ_11_2.py
@@ -0,0 +1,26 @@
+# Из предложенного текстового файла (text18-3.txt) вывести на экран его содержимое,
+# количество знаков пунктуации в первых четырёх строках. Сформировать новый файл, в
+# который поместить текст в стихотворной форме предварительно заменив символы третей
+# строки их числовыми кодами.
+
+import string
+
+with open('text18-3.txt', encoding='utf-16-le') as file:
+    text = file.read()
+
+print('Содержимое:')
+print(text)
+
+split = text.split('\n')
+punctuation = []
+for line in split[:4]:
+    for char in line:
+        if char in string.punctuation + '«»':
+            punctuation.append(char)
+print(f"\nКоличество знаков пунктуации: {len(punctuation)}")
+
+with open('new_file.txt', 'w', encoding='utf-16-le') as file:
+    split[2] = ''.join([str(ord(i)) for i in split[3]])
+    file.write('\n'.join(split))
+
+# 16 min
---
+++ PZ_12/PZ_12_2.py
@@ -0,0 +1,14 @@
+# Составить генератор (yield), который выводит из строки только цифры.
+
+def find_numbers(text: str) -> int:
+    for i in text:
+        if i.isdigit():
+            yield int(i)
+
+
+text = input('Введите строку: ')
+
+nums = find_numbers(text)
+print(list(nums))
+
+# 3 min
