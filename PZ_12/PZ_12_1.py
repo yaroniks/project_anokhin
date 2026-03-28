9ff72024c37870a4e4ae9fbf2ba0d3081c893619
yarovich <anokhin247yarik0@gmail.com>
2026-03-28 19:11:01 +0300

    add pz_12

---
+++ PZ_12/PZ_12_1.py
@@ -0,0 +1,35 @@
+# В квадратной матрице элементы на главной диагонали увеличить в 2 раза.
+
+import random
+
+n = int(input("Введите размер матрицы: "))
+
+
+def generate_matrix(n: int) -> list[list[int]]:
+    return [
+        [random.randint(-100, 100) for j in range(n)]
+        for i in range(n)
+    ]
+
+
+def change_main_diagonal(matrix: list[list[int]]) -> list[list[int]]:
+    return [
+        [matrix[i][j]*2 if i == j else matrix[i][j] for j in range(len(matrix))]
+        for i in range(len(matrix))
+    ]
+
+
+def print_matrix(matrix: list[list[int]]):
+    for i in matrix:
+        print(i)
+
+
+matrix = generate_matrix(n)
+
+print('Исходная матрица:')
+print_matrix(matrix)
+
+matrix = change_main_diagonal(matrix)
+
+print('\nГлавная диагональ увеличеная в 2 раза:')
+print_matrix(matrix)
