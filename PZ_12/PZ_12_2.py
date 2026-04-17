82d2e9c5c1bfd3f5f35be44edf7e9334202eaa22
yarovich <anokhin247yarik0@gmail.com>
2026-04-17 15:37:18 +0300

    add pz_13, update pz_12, add regex

--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -18,8 +18,8 @@ def generate_matrix(n: int) -> list[list[int]]:
 
 def sum_avg_matrix(matrix: list[list[int]]) -> tuple[list[int], int, float]:
     nums = []
-    for i in matrix:
-        for j in i:
+    for i in matrix:  # i - строка
+        for j in i:  # j - стобец
             if j > 0 and j % 2 == 0:
                 nums.append(j)
     return nums, sum(nums), sum(nums) / len(nums)
